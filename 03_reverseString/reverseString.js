2938b4a04b522b7fbc8d7eac99b86e7b76abb236
supreme-hub <shoryaagarwal831@gmail.com>
2022-02-19 17:52:51 +0530

    completed ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = "";
+    let x = str.length;
+    for (let i = x-1; i >= 0 ; i--) {
+        newStr += str[i];
+    }
+    return newStr;
 };
 
+let out = reverseString("hello");
+console.log(out)
 // Do not edit below this line
 module.exports = reverseString;
