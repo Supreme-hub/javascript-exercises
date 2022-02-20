47a6caebf5ef19f1d9eb3839f66a1d506829d762
supreme-hub <shoryaagarwal831@gmail.com>
2022-02-20 16:54:53 +0530

    completed ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(int) {
+  return Math.round(((int - 32) * 5/9)*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(int) {
+  return Math.round(((int * 9/5) + 32)* 10)/10;
 };
 
 // Do not edit below this line
