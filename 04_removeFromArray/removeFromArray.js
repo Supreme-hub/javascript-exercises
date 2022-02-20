4cbaed04d8c11b0a53780743c2a9ebe97910d3b1
supreme-hub <shoryaagarwal831@gmail.com>
2022-02-20 16:27:36 +0530

    completed ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a ,...args) {
+    for (let i = 0; i < args.length; i++) {
+        let x = a.indexOf(args[i]);
+        if (x !== -1) {
+            a.splice(x, 1);
+        }
+    }
+    return a;
 };
 
 // Do not edit below this line
