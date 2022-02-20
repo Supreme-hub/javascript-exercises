2535181a6248d7d7d27383b151b134ca83d803ea
supreme-hub <shoryaagarwal831@gmail.com>
2022-02-20 16:41:22 +0530

    completed ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let total = 0;
+    total += int1;
+    if (int1 < 0 || int2 < 0 || typeof int1 !== "number" || typeof int2 !== "number") {
+        return "ERROR";
+    }
+    if (int2 > int1) {
+        for (let i = 0; i < (int2 - int1 + 1); i++) {
+            total += i + 1;
+        }
+        total -= 1;
+    }
+    else if (int1 > int2) {
+        for (let i = 0; i < (int1 - int2 ); i++) {
+            total += i + 1;
+        }
+    }
+    return total;
 };
 
 // Do not edit below this line
